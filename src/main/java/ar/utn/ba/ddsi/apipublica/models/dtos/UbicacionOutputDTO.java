314a6260c2b31030cfb0678638e792425853e116
Aponte Lucas <laponte@frba.utn.edu.ar>
2025-12-04 20:59:09 -0300

    hechodetallefront2

--- src/main/java/ar/utn/ba/ddsi/apipublica/models/dtos/UbicacionOutputDTO.java
+++ src/main/java/ar/utn/ba/ddsi/apipublica/models/dtos/UbicacionOutputDTO.java
@@ -12,21 +12,21 @@ public class UbicacionOutputDTO {
     private long id_ubicacion;
     private float latitud;
     private float longitud;
-    private String prrovincia;
+    private String provincia;
     private String pais;
 
     public UbicacionOutputDTO(long id_ubicacion, float latitud, float longitud, String nombre, String pais) {
         this.id_ubicacion = id_ubicacion;
         this.latitud = latitud;
         this.longitud = longitud;
-        this.prrovincia = nombre;
+        this.provincia = nombre;
         this.pais = pais;
     }
     public UbicacionOutputDTO(Ubicacion ubicacion){
         this.id_ubicacion = ubicacion.getIdUbicacion();
         this.latitud = ubicacion.getLatitud();
         this.longitud = ubicacion.getLongitud();
-        this.prrovincia = ubicacion.getProvincia().getNombre();
+        this.provincia = ubicacion.getProvincia().getNombre();
         this.pais = ubicacion.getProvincia().getPais();
     }
 
